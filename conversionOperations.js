0eb77fae0e4cd4a414d7cda4e106f4efb4816c4a
raj1809 <rajtheking147@gmail.com>
2026-01-12 20:29:32 +0530

    Added day 2 programs

---
+++ conversionOperations.js
@@ -0,0 +1,29 @@
+let score = 20
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let score1 = "30"
+console.log(typeof score1);
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);
+
+// "33" --> converts to 33
+// "33abc" ---> converts to 33 but value is NaN
+// true -->  1; false -->  0
+
+let isLoggedIn = 1
+
+let booleanIsLoggeIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggeIn);
+
+// 1 --> true; 0 --> false
+// "hitesh" --> true; "" (empty) --> false
+
+let someNumber = 34
+
+let stringNum = String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
+
