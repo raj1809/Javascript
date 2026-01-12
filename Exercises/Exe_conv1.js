604cf8dd754adfe8d939e215edc01e5a0e115b51
raj1809 <rajtheking147@gmail.com>
2026-01-12 20:39:40 +0530

    Added day 2 programs again

---
+++ Exercises/Exe_conv1.js
@@ -0,0 +1,22 @@
+let num = "25"
+let convStr = Number("25")
+console.log(convStr);  // 25
+console.log(typeof convStr); // number
+
+let str = 100
+let convNum = String(100)
+console.log(convNum);  // 100
+console.log(typeof convNum); // str
+
+let poo = 1
+let conBool = Boolean(1)
+console.log(conBool); //  true
+console.log(typeof conBool); // boolean
+
+let boo = 0
+let convBool = Boolean(0)
+console.log(convBool);   // false
+console.log(typeof convBool); // boolean
+
+console.log("**********************************");
+
