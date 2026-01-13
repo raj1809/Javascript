536559190bdcd30201b4ac238ec0dc0bb37e6ce9
raj1809 <rajtheking147@gmail.com>
2026-01-13 20:37:48 +0530

    Added day 3 programs....

---
+++ Comparison.js
@@ -0,0 +1,34 @@
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("****************");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null <= 0);
+
+// conversion operators convert nulll to a number, treating it as 0 thats why null >= 0 is true and null > 0 is false
+console.log("*****************");
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+console.log("*****************");
+
+console.log(undefined == null);
+console.log(undefined > null);
+console.log(undefined < null);
+console.log(undefined >= null);
+console.log(undefined <= null);
+
+console.log("*****************");
+
+
+
+
