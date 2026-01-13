dbe161a0295529b3f17347f01f0a4f0bbb0070b4
raj1809 <rajtheking147@gmail.com>
2026-01-13 20:53:32 +0530

    Added day 3 programs......

---
+++ DataTypes_Summary.js
@@ -0,0 +1,17 @@
+// Primitive --> 7 types : String,  Number,  Boolean, null, undefined, Symbol, BigInt
+
+
+
+// Non-Primitive (Reference type) --> 3 types : Arrays,  Objects, Functions
+
+const cars = ["BMW", "LAND ROVER"] // array
+
+let myDetails = {                 // object
+        firstName : "John",
+        age : 22,
+}
+ 
+const myFunction = function() {            //  function
+        console.log("Hello JS!!!");
+}
+
