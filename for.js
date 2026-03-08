dea72ba2154dd3ae4b4b898598d317f391c16ff7
raj1809 <rajtheking147@gmail.com>
2026-03-08 18:37:48 +0530

    added for.js examples

---
+++ for.js
@@ -0,0 +1,31 @@
+// syntax
+// for (intialization; condition; inc/dec){
+//     code 
+//    }  --> it repeat some code A LIMITED number of times
+
+// example 1
+for (let i=2; i<=20; i+=2){
+    console.log(i); 
+}
+console.log("------------------------");
+
+// continue
+for(let i = 1; i <= 10; i++){
+    if (i == 3) {
+        continue;  // use to skip an iteration
+    }
+     else {
+        console.log(i);
+     }
+}
+console.log("------------------------");
+
+// break 
+for(let i = 1; i <= 10; i++){
+    if (i == 3) {
+        break;  // use to breqak out of loop entirely
+    }
+     else {
+        console.log(i);
+     }
+}
