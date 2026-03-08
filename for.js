332c576125b41d0ba5abc5896b01537242d4ed94
raj1809 <rajtheking147@gmail.com>
2026-03-08 18:39:38 +0530

    added examples of incremetn & decrement

--- for.js
+++ for.js
@@ -29,3 +29,15 @@ for(let i = 1; i <= 10; i++){
         console.log(i);
      }
 }
+console.log("------------------------");
+
+// / ------- INCREMENT -------
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+console.log("------------------------");
+
+// ------- DECREMENT -------
+for(let i = 10; i > 0; i--){
+    console.log(i);
+}
