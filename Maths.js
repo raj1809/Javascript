1dcf005c4490ee3041e07c82533c06e74f56f604
raj1809 <rajtheking147@gmail.com>
2026-03-14 18:47:47 +0530

    Added maths & Numbers

---
+++ Maths.js
@@ -0,0 +1,24 @@
+// console.log(Math)
+// console.log(Math.abs(-4567))
+// console.log(Math.round(4.4))
+// console.log(Math.round(4.6))
+
+// console.log(Math.floor(2.1))
+// console.log(Math.floor(2.9))
+
+// console.log(Math.ceil(2.1))
+// console.log(Math.ceil(2.9))
+
+// console.log(Math.sqrt(25))
+
+// console.log(Math.max(22, 4268, 9847, 100))
+// console.log(Math.min(22, 4268, 9847, 100))
+
+// console.log(Math.random()) // gives value between 0-1
+// console.log(Math.floor((Math.random() * 10)) + 1)
+// console.log(Math.ceil((Math.random() * 10)) + 1)
+
+const min = 10
+const max = 20
+ 
+console.log(Math.floor(Math.random(max - min) * 10) + min) // wil give value betwen 10 - 20
